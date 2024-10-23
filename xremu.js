f5900bd00368ba11ec4bd6f7293729b50463f724
Adnan Saad <the.lone.comrade.was.here@gmail.com>
2024-10-23 22:09:52 +1100

    Add files via upload

--- xremu.js
+++ xremu.js
@@ -35,7 +35,7 @@ if (ENVIRONMENT_IS_NODE) {
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: /tmp/tmp9lhs45po.js
+// include: /tmp/tmpethr6wlu.js
 
   if (!Module['expectedDataFileDownloads']) {
     Module['expectedDataFileDownloads'] = 0;
@@ -216,7 +216,7 @@ Module['FS_createPath']("/", "bin", true, true);
 
   })();
 
-// end include: /tmp/tmp9lhs45po.js
+// end include: /tmp/tmpethr6wlu.js
 
 
 // Sometimes an existing Module object exists with properties
